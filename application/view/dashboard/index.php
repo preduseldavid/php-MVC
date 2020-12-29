212bc0f3c0f235b4ee8a56c18cbd081542ddfd9b
David Predusel <preduseldavid@yahoo.com>
2020-12-29 12:49:53 +0200

    test commit

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -6,7 +6,6 @@
         <?php $this->renderFeedbackMessages(); ?>
 
         <h3>What happens here ?</h3>
-        <h2>What happens here 2 ?</h2>
         <p>
             This is an area that's only visible for logged in users. Try to log out, an go to /dashboard/ again. You'll
             be redirected to /index/ as you are not logged in. You can protect a whole section in your app within the
